edf766a21e0dd8d7393b90f004f1eddc62f4e56c
JAVIER ALEXANDER HERNANDEZ LEON <hernandezjavl@HILDEBOG.LOCAL>
2021-02-01 18:17:12 -0500

    New note view

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -4,4 +4,8 @@ router.get('/notes', (req, res) => {
     res.send('Notes from database');
 });
 
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note');
+});
+
 module.exports = router;
