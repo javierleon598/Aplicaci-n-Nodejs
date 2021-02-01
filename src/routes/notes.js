6dd6cc0795fa330f08fc893e733a77e97bb1951f
JAVIER ALEXANDER HERNANDEZ LEON <hernandezjavl@HILDEBOG.LOCAL>
2021-02-01 18:49:23 -0500

    Obteniendo Datos del formulario

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -8,4 +8,24 @@ router.get('/notes/add', (req, res) => {
     res.render('notes/new-note');
 });
 
+router.post('/notes/new-note', (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if(!title){
+        errors.push({text: 'Please Write a Title'})
+    }
+    if(!description){
+        errors.push({text: 'Please Write a Description'})
+    }
+    if(errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        })
+    } else {
+        res.send('OK');
+    }
+})
+
 module.exports = router;
