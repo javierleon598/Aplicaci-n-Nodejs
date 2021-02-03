dbc7d14f6fccfd3eb227ee56b506e5efa11045a5
JAVIER ALEXANDER HERNANDEZ LEON <hernandezjavl@HILDEBOG.LOCAL>
2021-02-03 09:35:26 -0500

    Insertando usuarios a MongoDB

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,45 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.get('/user/signin', (req, res) => {
+router.get('/users/signin', (req, res) => {
     res.render('users/signin');
 });
 
-router.get('/user/signup', (req, res) => {
+router.get('/users/signup', (req, res) => {
     res.render('users/signup');
+});
+
+router.post('/users/signup', async (req, res) => {
+    const { name, email, password, confirm_password } = req.body;
+    const errors = [];
+    if(name.length <= 0){
+        errors.push({text: 'Please Insert your name'});
+    }
+    if(email.length <= 0){
+        errors.push({text: 'Please Insert your email'});
+    }
+    if(password != confirm_password){
+        errors.push({text : 'Password do not match'});
+    }
+    if(password.length < 4){
+        errors.push({ text: 'Password must be at least 4 charaters'});
+    }
+    if(errors.length > 0){
+        console.log(errors);
+        res.render('users/signup', { errors, name, email, password, confirm_password } );
+    }else{
+        const emailUser = await User.findOne({email: email});
+        if(emailUser){
+            req.flash('error_msg', 'The Email is already in use');
+            res.redirect('/users/signup');
+        }else{
+            const newUser = new User({name, email, password});
+            console.log(newUser);
+            await newUser.save();
+            req.flash('success_msg', 'You are registered');
+            res.redirect('/users/signin');
+        }
+    }
 });
 
 module.exports = router;
